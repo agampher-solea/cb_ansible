393d0afd72e80da622b5fdfdd8886200af077cdb
Kevin Hamon <khamon@soleaenergy.com>
2023-08-02 00:25:09 -0400

    Working server install with rebalanced nodes

--- test/runTest.sh
+++ test/runTest.sh
@@ -2,5 +2,6 @@ ssh-add -D
 ssh-add ./.vagrant/machines/node1/virtualbox/private_key
 ssh-add ./.vagrant/machines/node2/virtualbox/private_key
 ssh-add ./.vagrant/machines/node3/virtualbox/private_key
+export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i ../inventories/local/hosts ../deploy_couchbase.yml
 
