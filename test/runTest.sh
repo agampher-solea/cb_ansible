3658ece2d6b0c2b30c3190f7641b7d7aaff2db81
Kevin Hamon <khamon@soleaenergy.com>
2023-08-03 22:18:59 -0400

    fully working playbook

--- test/runTest.sh
+++ test/runTest.sh
@@ -2,6 +2,7 @@ ssh-add -D
 ssh-add ./.vagrant/machines/node1/virtualbox/private_key
 ssh-add ./.vagrant/machines/node2/virtualbox/private_key
 ssh-add ./.vagrant/machines/node3/virtualbox/private_key
+ssh-add ./.vagrant/machines/node4/virtualbox/private_key
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i ../inventories/local/hosts ../deploy_couchbase.yml
 
