839a4326f48bdcdfbb2ba89740da0d1067246b2a
Kevin Hamon <khamon@soleaenergy.com>
2023-08-04 16:39:35 -0400

    Index wip

--- test/runTest.sh
+++ test/runTest.sh
@@ -4,5 +4,5 @@ ssh-add ./.vagrant/machines/node2/virtualbox/private_key
 ssh-add ./.vagrant/machines/node3/virtualbox/private_key
 ssh-add ./.vagrant/machines/node4/virtualbox/private_key
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i ../inventories/local/hosts ../deploy_couchbase.yml
+ansible-playbook -i ../inventories/local/hosts ../deploy_couchbase_local.yml
 
