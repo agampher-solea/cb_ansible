cfe765d25e3524d57f445f66215ab725c59a00bf
Kevin Hamon <khamon@soleaenergy.com>
2023-08-14 17:14:19 -0400

    Refactored users and added indexes

--- test/runTest.sh
+++ test/runTest.sh
@@ -5,4 +5,5 @@ ssh-add ./.vagrant/machines/node3/virtualbox/private_key
 ssh-add ./.vagrant/machines/node4/virtualbox/private_key
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i ../inventories/local/hosts ../deploy_couchbase_local.yml
+ansible-playbook -i ../inventories/local/hosts ../deploy_couchbase_indexes.yml
 
